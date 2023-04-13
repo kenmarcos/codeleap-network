021fde56a2053da7133c220f541fc96d697cbc6e
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-04-13 11:32:32 -0300

    feat: add layou of dialog to edit post

--- src/pages/Home/components/PostCard/index.tsx
+++ src/pages/Home/components/PostCard/index.tsx
@@ -4,8 +4,20 @@ import Trash from "@/icons/Trash";
 import { Post } from "@/redux/post/reducer";
 import { parseISO, formatDistanceToNow } from "date-fns";
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
+import * as Dialog from "@radix-ui/react-dialog";
 import { useTypedSelector } from "@/redux/hooks";
 import { User } from "@/redux/user/reducer";
+import { Form } from "@/components/Form";
+import * as yup from "yup";
+import { FormProvider, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+export const editPostSchema = yup.object().shape({
+  title: yup.string().required("*Required field"),
+  content: yup.string().required("*Required field"),
+});
+
+export type editPostData = yup.InferType<typeof editPostSchema>;
 
 interface PostCardProps {
   post: Post;
@@ -22,6 +34,23 @@ const PostCard = (props: PostCardProps) => {
 
   const handleDeleteBtnClick = () => {
     console.log(`Deletar post de id ${id}`);
+  };
+
+  const editPostForm = useForm<editPostData>({
+    resolver: yupResolver(editPostSchema),
+    defaultValues: {
+      title,
+      content,
+    },
+  });
+
+  const {
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = editPostForm;
+
+  const handleFormSubmit = (data: editPostData) => {
+    console.log(data);
   };
 
   return (
@@ -65,9 +94,63 @@ const PostCard = (props: PostCardProps) => {
             </AlertDialog.Portal>
           </AlertDialog.Root>
 
-          <Button>
-            <Edit />
-          </Button>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button>
+                <Edit />
+              </Button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className="bg-black bg-opacity-50 data-[state=open]:animate-overlayShow fixed inset-0" />
+
+              <Dialog.Content className="border border-custom-gray-500 data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[660px] translate-x-[-50%] translate-y-[-50%] rounded-2xl bg-white p-6 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+                <Dialog.Title className="mb-6">Edit item </Dialog.Title>
+
+                <FormProvider {...editPostForm}>
+                  <form
+                    onSubmit={handleSubmit(handleFormSubmit)}
+                    className="space-y-6"
+                  >
+                    <Form.Field>
+                      <Form.Label>Title</Form.Label>
+
+                      <Form.Input name="title" placeholder="Title here" />
+
+                      <Form.ErrorMessage error={errors.title?.message} />
+                    </Form.Field>
+
+                    <Form.Field>
+                      <Form.Label>Content</Form.Label>
+
+                      <Form.TextArea
+                        name="content"
+                        placeholder="Content here"
+                      />
+
+                      <Form.ErrorMessage error={errors.content?.message} />
+                    </Form.Field>
+
+                    <div className="flex justify-end gap-[25px]">
+                      <Dialog.Close asChild>
+                        <Button className="btn-outline-white w-[120px] h-8">
+                          Cancel
+                        </Button>
+                      </Dialog.Close>
+
+                      <Button
+                        type="submit"
+                        className="btn-success w-[120px] h-8"
+                        disabled={!isDirty}
+                      >
+                        Save
+                      </Button>
+                    </div>
+                  </form>
+                </FormProvider>
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </div>
         {/* )} */}
       </header>
